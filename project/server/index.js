d623173f665723077c3ee3d0e312d0c37b92c03f
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-07 17:24:30 +0200

    First display from server in browser

--- project/server/index.js
+++ project/server/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 
 const app = express();
 
+const router = express.Router();
+
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -16,4 +18,10 @@ const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
   console.log("Server is running on port" + port);
+});
+
+app.use("/test", router);
+
+router.get("/firstroute", (request, response) => {
+  response.send("this is the first info sent by the server");
 });
