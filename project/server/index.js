d92f7903a8f4c809de4ab1158c32f486552ac845
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-08 17:08:49 +0200

    Organised file

--- project/server/index.js
+++ project/server/index.js
@@ -8,25 +8,35 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
+const addMiddleware = () => {
+  app.use(express.json());
+  app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  app.use(cors());
+};
 
-app.use(cors());
+const startServer = () => {
+  const port = process.env.PORT || 5001;
+  app.listen(port, () => {
+    console.log("Server is running on port" + port);
+  });
+};
 
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => {
-  console.log("Server is running on port" + port);
-});
-console.log("Mongo DB credentials :>> ", process.env.DB);
-app.use("/devChat-City", chatPosts);
+const loadRoutes = () => {
+  app.use("/devChat-City", chatPosts);
+};
 
 const connectMongoDB = async () => {
   await mongoose.connect(process.env.DB);
   console.log("MongoDB is running!");
 };
-connectMongoDB();
+
+(async function controller() {
+  await connectMongoDB();
+  addMiddleware();
+  loadRoutes();
+  startServer();
+})();
