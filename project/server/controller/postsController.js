165ca690393a7dfc392053b31d8eaedf258782f4
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-10 21:33:04 +0200

    Get by coding lang/working with postman

--- project/server/controller/postsController.js
+++ project/server/controller/postsController.js
@@ -19,4 +19,26 @@ const getAllPosts = async (request, response) => {
     }
 }
 
-export { getAllPosts };
+const getPostsByCodingLanguage = async (request, response) => {
+    const codingLanguage = request.params.codingLanguage
+
+    try {
+        const requestedPostsByCodingLanguage = await postsModel.find({
+            codingLanguage: request.params.codingLanguage,
+        })
+        console.log('requestedPostsByCodingLanguage :>>>>>>>> ', requestedPostsByCodingLanguage);
+
+        response.status(200).json({
+            requestedPostsByCodingLanguage,
+            number: requestedPostsByCodingLanguage.length,
+            operationSuccess: "You are seeing a display of posts with the coding language that you have filtered by"
+        })
+    } catch (error) {
+        response.status(500).json({
+            errorMessage: "There was a error when trying trying to fetch this data!"
+        })
+        console.log('Error status 500, could not fetch data :>> ', error);
+    }
+};
+
+export { getAllPosts, getPostsByCodingLanguage };
--- project/server/routes/postsRouter.js
+++ project/server/routes/postsRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
 import postsModel from "../models/postsModel.js";
-import { getAllPosts } from "../controller/postsController.js";
+import { getAllPosts, getPostsByCodingLanguage } from "../controller/postsController.js";
 const router = express.Router();
 
 router.get("/all", getAllPosts);
+router.get("/:codingLanguage", getPostsByCodingLanguage);
+
 
 export default router;
