da81b9b2638787660f1e1e74a0a6aca22bb06100
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-10 17:10:47 +0200

    Deleted unused import

--- project/server/routes/usersRouter.js
+++ project/server/routes/usersRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
-import usersModel from "../models/usersModel.js";
 import { getAllUsers } from "../controller/usersController.js";
+
 const router = express.Router();
 
 router.get("/all", getAllUsers);
