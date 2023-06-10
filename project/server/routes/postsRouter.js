12b3df9ab36d58756485cd8f54cd859c8684e22f
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-10 08:13:45 +0200

    Added the response for allPosts

--- project/server/routes/postsRouter.js
+++ project/server/routes/postsRouter.js
@@ -5,7 +5,9 @@ const router = express.Router();
 router.get("/all", async (request, response) => {
   // response.send("You are recieving all of the posts!");
   const allPosts = await postsModel.find({});
-  console.log('response :>> ', allPosts);
+  response.json({
+    allPosts
+  })
 });
 
 export default router;
