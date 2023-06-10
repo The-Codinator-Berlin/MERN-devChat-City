87251ce83fa90b2d7fb7f41e7cda1a46ca9aa0d1
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-10 16:29:19 +0200

    Moved func to controller and called in route file

---
+++ project/server/controller/usersController.js
@@ -0,0 +1,21 @@
+import usersModel from "../models/usersModel.js";
+
+const getAllUsers = async (request, response) => {
+
+    try {
+        const allUsers = await usersModel.find({});
+        response.status(200).json({
+            allUsers,
+            number: allUsers.length,
+            operationSuccess: "You are seeing a display of all the users in devChat-City"
+        })
+    } catch (error) {
+        response.status(500).json({
+            errorMessage: "There was a error when trying trying to fetch the users data!"
+        })
+        console.log('Error status 500, could not fetch users data :>> ', error);
+    }
+
+}
+
+export { getAllUsers }; 
--- project/server/routes/usersRouter.js
+++ project/server/routes/usersRouter.js
@@ -1,23 +1,8 @@
 import express from "express";
 import usersModel from "../models/usersModel.js";
+import { getAllUsers } from "../controller/usersController.js";
 const router = express.Router();
 
-router.get("/all", async (request, response) => {
-
-    try {
-        const allUsers = await usersModel.find({});
-        response.status(200).json({
-            allUsers,
-            number: allUsers.length,
-            operationSuccess: "You are seeing a display of all the users in devChat-City"
-        })
-    } catch (error) {
-        response.status(500).json({
-            errorMessage: "There was a error when trying trying to fetch the users data!"
-        })
-        console.log('Error status 500, could not fetch users data :>> ', error);
-    }
-
-});
+router.get("/all", getAllUsers);
 
 export default router;
