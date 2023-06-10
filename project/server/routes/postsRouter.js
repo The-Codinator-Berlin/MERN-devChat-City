cc493c711a9a7562862b50471a4211b4d239a611
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-10 14:44:36 +0200

    Implemented error handling in users &  posts route

--- project/server/routes/postsRouter.js
+++ project/server/routes/postsRouter.js
@@ -3,13 +3,22 @@ import postsModel from "../models/postsModel.js";
 const router = express.Router();
 
 router.get("/all", async (request, response) => {
-  // response.send("You are recieving all of the posts!");
-  const allPosts = await postsModel.find({});
-  response.status(200).json({
-    allPosts,
-    number: allPosts.length,
-    description: "You are seeing all the posts that have been posted in devChat-City"
-  })
+
+  try {
+    const allPosts = await postsModel.find({});
+    response.status(200).json({
+      allPosts,
+      number: allPosts.length,
+      operationSuccess: "You are seeing a display of all the posts in devChat-City"
+    })
+    console.log('Successfully fetched, status 200 :>> ', response);
+
+  } catch (error) {
+    response.status(500).json({
+      errorMessage: "There was a error when trying trying to fetch this data!"
+    })
+    console.log('Error status 500, could not fetch data :>> ', error);
+  }
 });
 
 export default router;
---
+++ project/server/routes/usersRouter.js
@@ -0,0 +1,23 @@
+import express from "express";
+import usersModel from "../models/usersModel.js";
+const router = express.Router();
+
+router.get("/all", async (request, response) => {
+
+    try {
+        const allUsers = await usersMdel.find({});
+        response.status(200).json({
+            allUsers,
+            number: allUsers.length,
+            operationSuccess: "You are seeing a display of all the users in devChat-City"
+        })
+    } catch (error) {
+        response.status(500).json({
+            errorMessage: "There was a error when trying trying to fetch the users data!"
+        })
+        console.log('Error status 500, could not fetch users data :>> ', error);
+    }
+
+});
+
+export default router;
