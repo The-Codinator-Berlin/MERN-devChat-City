e8042bda4cc3f11c8547ae44da5ea7bce88a4351
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-11 14:27:26 +0200

    If/else for if there's nothing to show to user

--- project/server/controller/postsController.js
+++ project/server/controller/postsController.js
@@ -8,7 +8,7 @@ const getAllPosts = async (request, response) => {
             allPosts,
             number: allPosts.length,
             operationSuccess: "You are seeing a display of all the posts in devChat-City"
-        })
+        });
         console.log('Successfully fetched, status 200 :>> ', response);
 
     } catch (error) {
@@ -16,8 +16,8 @@ const getAllPosts = async (request, response) => {
             errorMessage: "There was a error when trying trying to fetch this data!"
         })
         console.log('Error status 500, could not fetch data :>> ', error);
-    }
-}
+    };
+};
 
 const getPostsByCodingLanguage = async (request, response) => {
     const codingLanguage = request.params.codingLanguage
@@ -25,20 +25,24 @@ const getPostsByCodingLanguage = async (request, response) => {
     try {
         const requestedPostsByCodingLanguage = await postsModel.find({
             codingLanguage: request.params.codingLanguage,
-        })
+        });
         console.log('requestedPostsByCodingLanguage :>>>>>>>> ', requestedPostsByCodingLanguage);
-
-        response.status(200).json({
-            requestedPostsByCodingLanguage,
-            number: requestedPostsByCodingLanguage.length,
-            operationSuccess: "You are seeing a display of posts with the coding language that you have filtered by"
-        })
+        if (requestedPostsByCodingLanguage.length === 0) {
+            response.status(200).json({
+                message: "Sorry there are no posts matching the coding language you are looking for!",
+            });
+        } else {
+            response.status(200).json({
+                requestedPostsByCodingLanguage,
+                number: requestedPostsByCodingLanguage.length,
+            });
+        };
     } catch (error) {
         response.status(500).json({
             errorMessage: "There was a error when trying trying to fetch this data!"
-        })
+        });
         console.log('Error status 500, could not fetch data :>> ', error);
-    }
+    };
 };
 
 export { getAllPosts, getPostsByCodingLanguage };
