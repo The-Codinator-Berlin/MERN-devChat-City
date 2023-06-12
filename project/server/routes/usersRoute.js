e7fbb90510fecbd0b5d1ee49ab8408d9928008aa
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-12 11:42:01 +0200

    multerUpload called in usersRoute

--- project/server/routes/usersRoute.js
+++ project/server/routes/usersRoute.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { getAllUsers } from "../controller/usersController.js";
 import { imageUpload } from "../controller/usersController.js"
+import multerUpload from "../middleware/multer.js";
 
 
 const userRoute = express.Router();
 
 userRoute.get("/all", getAllUsers);
 
-userRoute.post("/imageUpload", imageUpload);
+userRoute.post("/imageUpload", multerUpload.single("image"), imageUpload);
 
 export default userRoute;
