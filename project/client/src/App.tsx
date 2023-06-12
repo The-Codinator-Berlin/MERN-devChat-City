4289911c49caf7505f2483d60de1e4faa7b5be21
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-12 19:13:49 +0200

    Deleted useState

--- project/client/src/App.tsx
+++ project/client/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from "./views/LandingPage.tsx";
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Routes>
