4c3eb02e3fa15b2d94a774a33829a2fd0bb9d5bb
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-12 15:28:57 +0200

    imageUpload functional

--- project/server/controller/usersController.js
+++ project/server/controller/usersController.js
@@ -1,4 +1,6 @@
 import usersModel from "../models/usersModel.js";
+import { v2 as cloudinary } from "cloudinary";
+
 
 const getAllUsers = async (request, response) => {
 
@@ -13,14 +15,35 @@ const getAllUsers = async (request, response) => {
         response.status(500).json({
             errorMessage: "There was a error when trying trying to fetch the users data!"
         })
-        console.log('Error status 500, could not fetch users data :>> ', error);
+        console.log("\u001B[31m" + 'Error status 500, could not fetch users data :>> ', error);
     }
 
 }
 
 const imageUpload = async (request, response) => {
+    //NOTE - Upload image
+    if (request.file) {
+        try {
+            const uploadImage = await cloudinary.uploader.upload(request.file.path, {
+                folder: "devChat-City_imageUpload",
+            });
+            response.status(200).json({
+                message: "Your file has been uploaded successfully",
+                avatar: uploadImage.url
+            });
+            console.log("File uploaded successfully!")
+        } catch (error) {
+            console.log("\u001B[31m", error);
+        }
+    } else {
+        //NOTE - 415 refers to wrong format attemting to be uploaded
+        response.status(415).json({
+            message: "\u001B[31m" + "Sorry but this file type is not supported! !status 415!)"
+        });
+        console.log("File not supported!");
+    };
 
-}
+};
 
 
 
