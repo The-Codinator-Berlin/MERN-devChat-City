b6c2541a5e07d61b53285cc9f7f78c1c11ab0305
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-13 15:48:13 +0200

    Further styling and link to about page

--- project/client/src/views/LandingPage.tsx
+++ project/client/src/views/LandingPage.tsx
@@ -1,21 +1,35 @@
-import devChatLogo from '../assets/devChatLogo.jpeg';
+import { Link } from "react-router-dom";
+import devChatLogo from "../assets/devChatLogo.jpeg";
 
 const LandingPage = () => {
     return (
         <div className="grid h-screen place-items-center bg-black text-white ">
-            <img className='md:w-80 h-80 rounded-b-full' src={devChatLogo} alt="Image" />
+            <img
+                className="md:w-80 h-80 rounded-b-full"
+                src={devChatLogo}
+                alt="Image"
+            />
             <div className="flex justify-center">
-                <h1 className="text-center font-Monoton md:text-6xl sm:text-xs">devChat-City</h1>
+                <h1 className="text-center font-Monoton md:text-6xl sm:text-xs">
+                    devChat-City
+                </h1>
                 <div className="w-40 h-20 flex justify-around font-Poppins font-extralight md:flex-row sm:flex-wrap">
                     <button>Login</button>
                     <button>Register</button>
                 </div>
             </div>
             <br />
-            <h6 className='font-poppins font-extralight text-xs text-sky-400'>Designed and built by <a href="">Carl Manley</a></h6>
-
+            <h6 className="font-poppins font-extralight text-xs text-sky-400">
+                Designed and developed by{" "}
+                <Link
+                    to="/devChat-City/api/about"
+                    className="hover:text-orange-500 hover:text-lg"
+                >
+                    Carl Manley
+                </Link>
+            </h6>
         </div>
-    )
-}
+    );
+};
 
 export default LandingPage;
