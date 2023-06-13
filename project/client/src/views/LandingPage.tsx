99936f04c43c3c08ddb90b8b12dd8631281497be
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-13 14:28:32 +0200

    Landing page created with login/Reg button

--- project/client/src/views/LandingPage.tsx
+++ project/client/src/views/LandingPage.tsx
@@ -1,10 +1,19 @@
-
-type Props = {}
+import devChatLogo from '../assets/devChatLogo.jpeg';
 
 const LandingPage = () => {
     return (
-        <div>
-            <h1 className="text-green-600">Hello</h1>
+        <div className="grid h-screen place-items-center bg-black text-white ">
+            <img className='md:w-80 h-80 rounded-b-full' src={devChatLogo} alt="Image" />
+            <div className="flex justify-center">
+                <h1 className="text-center font-Monoton md:text-6xl sm:text-xs">devChat-City</h1>
+                <div className="w-40 h-20 flex justify-around font-Poppins font-extralight md:flex-row sm:flex-wrap">
+                    <button>Login</button>
+                    <button>Register</button>
+                </div>
+            </div>
+            <br />
+            <h6 className='font-poppins font-extralight text-xs text-sky-400'>Designed and built by <a href="">Carl Manley</a></h6>
+
         </div>
     )
 }
