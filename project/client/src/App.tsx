800914be2b150313cbd1e3ec24fc43e644c5ad9a
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-13 19:48:43 +0200

    Paths added for login/register

--- project/client/src/App.tsx
+++ project/client/src/App.tsx
@@ -1,8 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from "./views/LandingPage.tsx";
 import AboutPage from "./views/AboutPage.tsx";
-
+import Login from './views/Login.tsx';
+import Register from './views/Register.tsx';
 import './App.css'
+
+
 
 function App() {
 
@@ -10,6 +13,8 @@ function App() {
     <Routes>
       <Route path="/devChat-City/api" element={<LandingPage />} />
       <Route path="/devChat-City/api/about" element={<AboutPage />} />
+      <Route path="/devChat-City/api/login" element={<Login />} />
+      <Route path="/devChat-City/api/register" element={<Register />} />
     </Routes>
   );
 }
