58ec74d45db0820137023b689e6d15b74b63dc3c
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-14 21:46:43 +0200

    Route updated to register user

--- project/server/routes/usersRoute.js
+++ project/server/routes/usersRoute.js
@@ -1,13 +1,16 @@
 import express from "express";
-import { getAllUsers } from "../controller/usersController.js";
+import { getAllUsers, registerUser } from "../controller/usersController.js";
 import { imageUpload } from "../controller/usersController.js"
 import multerUpload from "../middleware/multer.js";
 
 
 const userRoute = express.Router();
 
+//Get requests to MongoDB
 userRoute.get("/all", getAllUsers);
 
+//Post requests to MongoDB
+userRoute.post("/register", registerUser);
 userRoute.post("/imageUpload", multerUpload.single("image"), imageUpload);
 
 export default userRoute;
