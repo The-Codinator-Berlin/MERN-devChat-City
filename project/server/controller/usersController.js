229a08ddb74e255643212bcf94227bba658cd30e
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-14 21:47:27 +0200

    Added register user func

--- project/server/controller/usersController.js
+++ project/server/controller/usersController.js
@@ -1,5 +1,6 @@
 import usersModel from "../models/usersModel.js";
 import { v2 as cloudinary } from "cloudinary";
+import { hashedPassword } from "../utilities/encryptPassword.js";
 
 
 const getAllUsers = async (request, response) => {
@@ -43,10 +44,57 @@ const imageUpload = async (request, response) => {
         console.log("File not supported!");
     };
 
+
+
 };
 
+const registerUser = async (request, response) => {
+    console.log('request.body :>> ', request.body);
+    //First checking if user is in DB
+    try {
+        const existingUser = await usersModel.findOne({ email: request.body.email, userName: request.body.userName });
+        if (!existingUser) {
+            //If user doesn't exist we store that info
+            try {
+                const encryptPassword = await hashedPassword(request.body.password);
+                if (encryptPassword) {
+                    const newUser = new usersModel({
+                        userName: request.body.userName,
+                        email: request.body.email,
+                        password: encryptPassword,
+                        avatar: request.body.avatar,
+                    });
+                    try {
+                        const savedNewUser = await newUser.save();
+                        response.status(200).json({
+                            userName: savedNewUser.userName,
+                            email: savedNewUser.email,
+                            password: savedNewUser.password,
+                            avatar: savedNewUser.avatar
+                        });
+                        console.log('savedNewUser :>> ', savedNewUser);
+                    } catch (error) {
+                        console.log("\u001B[31m" + 'Error saving new user!', error);
+                    };
+                };
+            } catch (error) {
+                console.log('There was an error saving the user in the database! :>>', error);
+                response.status(500).json({
+                    errorMessage: "There was an error saving the user in the database!",
+                })
+            }
+        } else {
+            response.status(409).json({
+                errorMessage: "Sorry but there is already a registered account with that email address!"
+            })
+        }
+    } catch (error) {
+        console.log("\u001B[31m" + 'Unable to register user with this emeail address!', error);
+
+    }
+
+}
 
 
 
-
-export { getAllUsers, imageUpload, }; 
+export { getAllUsers, imageUpload, registerUser }; 
