adb3e89ba5733c58603a65c99db91fb65292ac43
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-15 20:46:28 +0200

    Formatted

--- project/server/index.js
+++ project/server/index.js
@@ -2,8 +2,8 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
-import postsRoute from "./routes/postsRoute.js"
-import usersRoute from "./routes/usersRoute.js"
+import postsRoute from "./routes/postsRoute.js";
+import usersRoute from "./routes/usersRoute.js";
 import cloudinaryConfig from "./config/cloudinary.js";
 
 dotenv.config();
@@ -18,7 +18,7 @@ const addMiddleware = () => {
     })
   );
   app.use(cors());
-  cloudinaryConfig()
+  cloudinaryConfig();
 };
 
 const startServer = () => {
