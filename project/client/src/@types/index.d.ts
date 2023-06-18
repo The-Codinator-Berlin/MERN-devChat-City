ce9c3eb022f3de4a54bed0b095ade58ced66c518
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-18 15:38:31 +0200

    Formatted

--- project/client/src/@types/index.d.ts
+++ project/client/src/@types/index.d.ts
@@ -1,11 +1,37 @@
-interface RegisterCredentials {
-  userName: string;
-  email: string;
-  password: string;
-    avatar: string;
+interface User {
+  userName: string,
+  email: string,
+  avatar: string,
+  token: string
 }
+
+interface RegisterCredentials {
+  userName: string,
+  email: string,
+  password: string,
+  avatar: string
+}
+
+interface FetchedUploadedFile {
+  avatar: string
+}
+
+// interface FetchedRegistrationResult{
+
+// }
 
 interface LoginCredentials {
   email: string;
-  password: string;
+  password: string
 }
+
+interface FetchedLoginResult {
+  message: string,
+  user: User,
+  token: string
+}
+
+
+
+
+
