72f9e8bc95eff9987e4a7da225162b0108386d2b
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-18 14:32:37 +0200

    Get userProfile route added

--- project/server/routes/usersRoute.js
+++ project/server/routes/usersRoute.js
@@ -1,13 +1,18 @@
 import express from "express";
-import { getAllUsers, loginUser, registerUser } from "../controller/usersController.js";
-import { imageUpload } from "../controller/usersController.js"
+import {
+  getAllUsers,
+  loginUser,
+  registerUser,
+  getUserProfile,
+} from "../controller/usersController.js";
+import { imageUpload } from "../controller/usersController.js";
 import multerUpload from "../middleware/multer.js";
-
 
 const userRoute = express.Router();
 
 //Get requests to MongoDB
 userRoute.get("/all", getAllUsers);
+userRoute.get("/userProfile", getUserProfile);
 
 //Post requests to MongoDB
 userRoute.post("/register", registerUser);
