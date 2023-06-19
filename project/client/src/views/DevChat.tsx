f6b38564267ce647bf280c223f240c8178a63055
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-19 14:24:14 +0200

    More responsive & some further styling for select

--- project/client/src/views/DevChat.tsx
+++ project/client/src/views/DevChat.tsx
@@ -7,7 +7,7 @@ import logo from "../assets/pictures/devChatLogo.jpeg";
 function DevChat() {
   return (
     <div className="bg-black text-white">
-      <div className="h-[100vh]  sm:h-screen border-y-[2px] border-white">
+      <div className="h-[100vh] sm:h-screen border-y-[2px] border-white">
         <div className="flex flex-row">
           <div className="flex flex-row flex-wrap justify-center">
             <div className="p-2">
@@ -16,9 +16,11 @@ function DevChat() {
                 devChat
               </h1>
             </div>
-            <div className="p-2 invisible sm:visible">
-              <h1 className="font-Monoton text-5xl">devChat</h1>
-              <h1 className="font-Monoton text-5xl rotate-180	">devChat</h1>
+            <div className="p-2">
+              <h1 className="text-2xl font-Monoton sm:text-5xl">devChat</h1>
+              <h1 className="text-2xl font-Monoton sm:text-5xl rotate-180	">
+                devChat
+              </h1>
             </div>
             {/* ------------NAV--------------> */}
             <div className=" w-full mb-4">
@@ -27,7 +29,7 @@ function DevChat() {
             {/* -----------------------------> */}
             <div className="w-full">
               {/* avatar box/ username... ------------>*/}
-              <div className="border-y-[0.1em] border-white h-40 w-full flex justify-around [align-items:center]">
+              <div className="border-y-[0.1em] border-white sm:h-40 w-screen sm:flex sm:justify-around sm:[align-items:center]">
                 <div className="flex justify-center">
                   <img
                     className="w-[8em] h-[8em] rounded-b-full"
@@ -35,32 +37,24 @@ function DevChat() {
                     alt="Image"
                   />
                 </div>
-                <div className="border-2 border-white flex flex-row justify-between px-8 [align-items:center]">
-                  <div className="text-sky-400">
-                    {/* <h3>{"<userName/>"}:</h3>
-                    <h3>{"<name/>"}:</h3>
-                    <h3>{"<email/>"}:</h3>
-                    <h3>{"<gitHub/>"}:</h3> */}
-                  </div>
-                  <div>
-                    <div className="border-white w-80 items-center flex flex-col justify-end">
-                      <select
-                        className="text-black"
-                        Search-by-common-coding-languages
-                      >
-                        <option value="Select a coding language">
-                          Select a coding language
-                        </option>
-                        <option value="javascript">JavaScript</option>
-                        <option value="python">Python</option>
-                        <option value="cpp">C++</option>
-                        <option value="java">Java</option>
-                        <option value="html">HTML</option>
-                        <option value="css">CSS</option>
-                        <option value="dart">Dart</option>
-                        <option value="typescript">TypeScript</option>
-                      </select>
-                    </div>
+                <div className="flex justify-center h-[1em] items-center my-4 rounded-full">
+                  <div className="border-2 w-[19.4.2em] flex justify-center [align-items:center] my-4 rounded-full">
+                    <select
+                      className="text-black bg-emerald-500 rounded-full"
+                      Search-by-common-coding-languages
+                    >
+                      <option value="filterByLanguage">
+                        Filter posts by popular coding languages{" "}
+                      </option>
+                      <option value="javascript">JavaScript</option>
+                      <option value="python">Python</option>
+                      <option value="cpp">C++</option>
+                      <option value="java">Java</option>
+                      <option value="html">HTML</option>
+                      <option value="css">CSS</option>
+                      <option value="dart">Dart</option>
+                      <option value="typescript">TypeScript</option>
+                    </select>
                   </div>
                 </div>
               </div>
