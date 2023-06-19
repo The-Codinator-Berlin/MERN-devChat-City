c6d6bafe738084b73b71c4b1658cd3d0916656e7
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-19 15:40:52 +0200

    Radio buttons added

--- project/client/src/views/DevChat.tsx
+++ project/client/src/views/DevChat.tsx
@@ -29,7 +29,7 @@ function DevChat() {
             {/* -----------------------------> */}
             <div className="w-full">
               {/* avatar box/ username... ------------>*/}
-              <div className="border-y-[0.1em] border-white sm:h-40 w-screen sm:flex sm:justify-around sm:[align-items:center]">
+              <div className="sm:border-y-[0.1em] sm:border-white sm:h-40 w-screen sm:flex sm:justify-around sm:[align-items:center]">
                 <div className="flex justify-center">
                   <img
                     className="w-[8em] h-[8em] rounded-b-full"
@@ -37,24 +37,66 @@ function DevChat() {
                     alt="Image"
                   />
                 </div>
-                <div className="flex justify-center h-[1em] items-center my-4 rounded-full">
-                  <div className="border-2 w-[19.4.2em] flex justify-center [align-items:center] my-4 rounded-full">
-                    <select
-                      className="text-black bg-emerald-500 rounded-full"
-                      Search-by-common-coding-languages
-                    >
-                      <option value="filterByLanguage">
-                        Filter posts by popular coding languages{" "}
-                      </option>
-                      <option value="javascript">JavaScript</option>
-                      <option value="python">Python</option>
-                      <option value="cpp">C++</option>
-                      <option value="java">Java</option>
-                      <option value="html">HTML</option>
-                      <option value="css">CSS</option>
-                      <option value="dart">Dart</option>
-                      <option value="typescript">TypeScript</option>
-                    </select>
+                <div>
+                  <div className="py-4">
+                    <label className="px-4" htmlFor="general">
+                      General
+                    </label>
+                    <input
+                      className="bg-emerald-500"
+                      type="radio"
+                      value="general"
+                      name="general"
+                    ></input>
+                  </div>
+
+                  <div className="py-4">
+                    <label className="px-4" htmlFor="general">
+                      allPosts
+                    </label>
+                    <input
+                      className="bg-emerald-500"
+                      type="radio"
+                      value="all"
+                      name="all"
+                    ></input>
+                  </div>
+
+                  <div className="py-4">
+                    <label className="px-4" htmlFor="general">
+                      Meetup
+                    </label>
+                    <input type="radio" value="meetup" name="general"></input>
+                  </div>
+                </div>
+                <div>
+                  <div className="flex justify-center flex-col h-[1em] items-center my-12 rounded-full">
+                    <div className="w-[19.4.2em] flex justify-center [align-items:center] my-4 rounded-full">
+                      <select
+                        className="text-black bg-emerald-500 rounded-full"
+                        Search-by-common-coding-languages
+                      >
+                        <option value="filterByLanguage">
+                          Filter posts by popular coding languages{" "}
+                        </option>
+                        <option value="javascript">JavaScript</option>
+                        <option value="python">Python</option>
+                        <option value="cpp">C++</option>
+                        <option value="java">Java</option>
+                        <option value="html">HTML</option>
+                        <option value="css">CSS</option>
+                        <option value="dart">Dart</option>
+                        <option value="typescript">TypeScript</option>
+                      </select>
+                    </div>
+                    <div className="flex justify-center items-center my-4 bg-emerald-500 rounded-full">
+                      <label className="px-2 text-black">Search: </label>
+                      <input
+                        type="search"
+                        placeholder="How can I..............?"
+                        className="placeholder-white bg-emerald-500 w-[15em] rounded-full text-center"
+                      ></input>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -62,13 +104,6 @@ function DevChat() {
             </div>
           </div>
         </div>
-
-        {/* <div className="flex-col items-end h-60">
-          <div className="grid col-span-full border-2 border-white">
-            <div>jhsgjhsgj</div>
-          </div>
-
-        </div> */}
       </div>
     </div>
   );
