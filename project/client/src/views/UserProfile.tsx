2809ed3da7420c2b414f1278bc39a9f952dd922c
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-20 11:54:08 +0200

    Added func to load user profile on load from DB

--- project/client/src/views/UserProfile.tsx
+++ project/client/src/views/UserProfile.tsx
@@ -1,11 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavigationBar from "../components/NavigationBar";
 import avatar from "../assets/pictures/avatar_default.jpeg";
 import logo from "../assets/pictures/devChatLogo.jpeg";
+import UserStatusfromToken from "../utilities/UserStatusfromToken";
+import { Navigate, useNavigate } from "react-router-dom";
 
 // type Props = {};
 
 function UserProfile() {
+  const [user, setUser] = useState<User>({
+    userName: "",
+    email: "",
+    avatar: "",
+    token: "",
+  });
+
+  const [error, setError] = useState<ResponseError>(null);
+  const goTo = useNavigate();
+
+  const loadUserProfile = async () => {
+    const storedToken = UserStatusfromToken();
+
+    if (storedToken) {
+      const myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${storedToken}`);
+
+      const requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+      };
+
+      try {
+        const response = await fetch(
+          "http://localhost:5001/api/devChat-City/users/userProfile",
+          requestOptions
+        );
+        console.log("response :>>>>>>>>> ", response);
+
+        if (response.ok) {
+          const result: FetchedProfileResult = await response.json();
+          setUser(result.user);
+          setError(null);
+        } else {
+          const result: FetchError = await response.json();
+          setError(result.error);
+          goTo("/devChat-City/api/errorRedirectToLogin");
+        }
+      } catch (error) {
+        console.log("Error :>> ", error);
+      }
+    }
+  };
+
+  useEffect(() => {
+    loadUserProfile();
+  }, []);
+
   return (
     <div className="bg-black text-white">
       <div className="h-[100vh]  md:h-screen border-y-[2px] border-white">
@@ -27,7 +77,7 @@ function UserProfile() {
             </div>
             <div className="w-full">
               {/* avatar box/ username... ------------>*/}
-              <div className="md:h-[12em] xl:h-[4] flex flex-wrap border-y-[0.1em] border-white sm:w-full md:flex justify-around [align-items:center]">
+              <div className="md:h-[12em] xl:h-[14] flex flex-wrap border-y-[0.1em] border-white sm:w-full md:flex justify-around [align-items:center]">
                 <img
                   className="w-[8em] h-[8em] border-white border-[0.1em] rounded-full"
                   src={avatar}
@@ -59,38 +109,98 @@ function UserProfile() {
                     alt="Image"
                   />
                 </div>
-                <div className="border-2 border-white flex-col md:flex md:flex-row justify-between md:px-8 [align-items:center]">
-                  <div className="text-sky-400">
-                    <h3>{"<userName/>"}:</h3>
-                    <h3>{"<name/>"}:</h3>
-                    <h3>{"<email/>"}:</h3>
-                    <h3>{"<gitHub/>"}:</h3>
-                  </div>
-                  <div>
-                    <div className=" border-white w-80 items-center flex flex-col justify-end">
-                      <h3>{"codeMan"}</h3>
-                      <h3>{"Peter Pan"}</h3>
-                      <h3>{"email@email.com"}</h3>
-                      <h3>{"https://github.com/The-Codinator-Berlin"}</h3>
+                <div className="border-2 border-white flex-col md:flex md:flex-row justify-between md:px-8 [align-items:center] w-[25em] h-[6.5em]">
+                  <div className="text-sky-400 flex justify-start flex-col w-[25em]">
+                    <div></div>
+                    <div className="flex justify-center">
+                      <div>
+                        <h3>
+                          {"<userName/>"}
+                          <span className="text-white">
+                            &nbsp;&nbsp;:&nbsp;&nbsp;
+                          </span>
+                        </h3>
+                      </div>
+                      <div>
+                        {user && (
+                          <h3 className="text-orange-500">{user.userName}</h3>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="flex justify-center">
+                      <div>
+                        <h3>
+                          {"<email/>"}
+                          <span className="text-white">
+                            &nbsp;&nbsp;:&nbsp;&nbsp;
+                          </span>
+                        </h3>
+                      </div>
+                      <div>
+                        {user && (
+                          <h3 className="text-orange-500">{user.email}</h3>
+                        )}
+                      </div>
+                    </div>
+
+                    <div className="flex justify-center">
+                      <div>
+                        <h3>
+                          {"<gitHub/>"}
+                          <span className="text-white">
+                            &nbsp;&nbsp;:&nbsp;&nbsp;
+                          </span>
+                        </h3>
+                      </div>
+                      {user && (
+                        <div>
+                          <h3 className="text-orange-500">{user.gitHub}</h3>
+                        </div>
+                      )}
+                      {user && (
+                        <div>
+                          {
+                            <h3 className="text-white">
+                              {"No registered gitHub currently"}
+                            </h3>
+                          }
+                        </div>
+                      )}
                     </div>
                   </div>
                 </div>
+
+                {/* <h3>{"}:</h3>
+                    <h3>{"}:</h3>
+                    <h3>{"<gitHub/>"}:</h3>
+                  </div> */}
+                {/* <div>
+                    {user && (
+                      <div className=" border-white w-80 items-center flex flex-col justify-end">
+                        <h3>{user.userName}</h3>
+                        <h3></h3>
+                        <h3>{user.email}</h3>
+                        <h3></h3>
+                      </div>
+                    )}
+                  </div> */}
               </div>
-              {/* ---------------------------------------> */}
-              <div className="flex justify-center font-Poppins text-2xl text-amber-200 font-light">
-                <h2>m_y-P_o_s_t_s:</h2>
-              </div>
+            </div>
+            {/* ---------------------------------------> */}
+            <div className="flex justify-center font-Poppins text-2xl text-amber-200 font-light">
+              <h2>m_y-P_o_s_t_s:</h2>
             </div>
           </div>
         </div>
+      </div>
 
-        {/* <div className="flex-col items-end h-60">
+      {/* <div className="flex-col items-end h-60">
           <div className="grid col-span-full border-2 border-white">
             <div>jhsgjhsgj</div>
           </div>
           
         </div> */}
-      </div>
     </div>
   );
 }
