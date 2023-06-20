77b6c50810e59d1ca79a810075f6083da8de1158
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-20 19:14:23 +0200

    Protected route for userProfile

--- project/client/src/App.tsx
+++ project/client/src/App.tsx
@@ -8,22 +8,33 @@ import "./App.css";
 import Loading from "./components/loading/LoadingOnLog.tsx";
 import DevChat from "./views/devChat.tsx";
 import ErrorRedirectToLogin from "./components/loading/ErrorRedirectToLogin.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx";
+import ProtectedRoute from "./routes/ProtectedRoute.tsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/devChat-City/api" element={<LandingPage />} />
-      <Route path="/devChat-City/api/about" element={<AboutPage />} />
-      <Route path="/devChat-City/api/login" element={<Login />} />
-      <Route path="/devChat-City/api/register" element={<Register />} />
-      <Route path="/devChat-City/api/userProfile" element={<UserProfile />} />
-      <Route path="/devChat-City/api/devchat" element={<DevChat />} />
-      <Route path="/devChat-City/api/loading" element={<Loading />} />
-      <Route
-        path="/devChat-City/api/errorRedirectToLogin"
-        element={<ErrorRedirectToLogin />}
-      />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/devChat-City/api" element={<LandingPage />} />
+        <Route path="/devChat-City/api/about" element={<AboutPage />} />
+        <Route path="/devChat-City/api/login" element={<Login />} />
+        <Route path="/devChat-City/api/register" element={<Register />} />
+        <Route
+          path="/devChat-City/api/userProfile"
+          element={
+            <ProtectedRoute>
+              <UserProfile />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/devChat-City/api/devchat" element={<DevChat />} />
+        <Route path="/devChat-City/api/loading" element={<Loading />} />
+        <Route
+          path="/devChat-City/api/errorRedirectToLogin"
+          element={<ErrorRedirectToLogin />}
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
 
