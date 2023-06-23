9b7e385ceaf3da478e11d2a38003ffd77e1896ab
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-23 11:42:04 +0200

    Added authentication for all users

--- project/server/routes/usersRoute.js
+++ project/server/routes/usersRoute.js
@@ -12,14 +12,19 @@ import authenticateJwt from "../middleware/jwtAuth.js";
 const userRoute = express.Router();
 
 //Get requests to MongoDB
-userRoute.get("/all", getAllUsers);
+userRoute.get("/all", authenticateJwt, getAllUsers);
 userRoute.get("/userProfile", authenticateJwt, getUserProfile);
 
 //Post requests to MongoDB
 userRoute.post("/register", registerUser);
 userRoute.post("/login", loginUser);
 userRoute.post("/imageUpload", multerUpload.single("image"), imageUpload);
-userRoute.post("/updateAvatar", multerUpload.single("image"), imageUpload);
+userRoute.post(
+  "/updateAvatar",
+  authenticateJwt,
+  multerUpload.single("image"),
+  imageUpload
+);
 // userRoute.post("/updateUserInfo", multerUpload.single("image"), imageUpload);
 
 export default userRoute;
