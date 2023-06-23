44590dfd5b3f41cdd1eab86f5bc8158ee2bfee8a
The-Codinator-Berlin <carl.manley.berlin@gmail.com>
2023-06-23 11:44:24 +0200

    Create post route created

--- project/server/routes/postsRoute.js
+++ project/server/routes/postsRoute.js
@@ -1,10 +1,22 @@
 import express from "express";
-import { getAllPosts, getPostsByCodingLanguage } from "../controller/postsController.js";
+import {
+  getAllPosts,
+  getPostsByCodingLanguage,
+} from "../controller/postsController.js";
+import authenticateJwt from "../middleware/jwtAuth.js";
+import multerUpload from "../middleware/multer.js";
+import { screenshotUpload } from "../controller/postsController.js";
 
 const PostsRoute = express.Router();
 
 PostsRoute.get("/all", getAllPosts);
-PostsRoute.get("/:codingLanguage", getPostsByCodingLanguage);
+PostsRoute.get("/:codingLanguage", authenticateJwt, getPostsByCodingLanguage);
 
+PostsRoute.post(
+  "/createPost",
+  authenticateJwt,
+  multerUpload.single("image"),
+  screenshotUpload
+);
 
 export default PostsRoute;
